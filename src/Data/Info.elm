370ed02d72a504ce7298d881257b4d9ea53c4756
Luis Pedro Coelho <luis@luispedro.org>
2026-05-12 21:21:55 +1000

    DAT Convert to urban soil

--- src/Data/Info.elm
+++ src/Data/Info.elm
@@ -5,56 +5,58 @@ module Data.Info exposing (
 
 
 datasetName : String
-datasetName = "Shanghai Dog Gut MAGs"
+datasetName = "Urban Soil MAGs"
 
 datasetTag : String
-datasetTag = "SHD1"
+datasetTag = "CS1"
 
 datasetSlug : String
-datasetSlug = "sh-dogs"
+datasetSlug = "urban-soil"
 
 baseURL : String
-baseURL = "https://sh-dog-mags-data.big-data-biology.org/"
+baseURL = "https://urban-soil-mags-data.big-data-biology.org/"
 
 mkFASTALink : String -> String
 mkFASTALink mid =
-    baseURL ++ "ShanghaiDogsMAGs/" ++ mid ++ ".fna.gz"
+    baseURL ++ "UrbanSoilMAGs/" ++ mid ++ ".fna.gz"
 
 mkENOGLink : String -> String
 mkENOGLink mid =
-    baseURL ++ "ShanghaiDogsMAGAnnotations/EMapper/" ++ mid ++ ".emapper.annotations.xz"
+    baseURL ++ "UrbanSoilMAGAnnotations/EMapper/" ++ mid ++ ".emapper.annotations.xz"
 
 mkEMapperSummaryLink : String -> String
 mkEMapperSummaryLink mid =
-    baseURL ++ "ShanghaiDogsMAGAnnotations/EMapperSummary/" ++ mid ++ ".emapper_summary.tsv"
+    baseURL ++ "UrbanSoilMAGAnnotations/EMapperSummary/" ++ mid ++ ".emapper_summary.tsv"
 
 mkBarrnapLink : String -> String
 mkBarrnapLink mid =
-    baseURL ++ "ShanghaiDogsMAGAnnotations/Barrnap/" ++ mid ++ "_ribosomal.fna.gz"
+    baseURL ++ "UrbanSoilMAGAnnotations/Barrnap/" ++ mid ++ "_ribosomal.fna.gz"
 
 mkReadmeFile : String -> String -> String -> String
 mkReadmeFile subtitle mid table =
-    "# Shanghai Dog Gut MAGs: " ++ subtitle
+    "# Urban Soil MAGs: " ++ subtitle ++ "\n"
         ++ "\n"
         ++ "This archive contains metagenome-assembled genomes (MAGs) from the\n"
-        ++ "Shanghai Dog Gut MAG catalogue, " ++ mid ++ ".\n"
+        ++ "Urban Soil MAG catalogue, " ++ mid ++ ".\n"
         ++ "\n"
         ++ "## Contents\n"
         ++ "\n"
         ++ "- `*.fna.gz` - Genome FASTA files (gzip-compressed)\n"
-        ++ "- `"++ table ++ ".metadata.tsv` - MAG metadata (TSV format)\n"
+        ++ "- `" ++ table ++ ".metadata.tsv` - MAG metadata (TSV format)\n"
         ++ "- `README.md` - This file\n"
         ++ "\n"
         ++ "## Source\n"
         ++ "\n"
-        ++ "Data downloaded from the Shanghai Dog Gut MAG Viewer:\n"
-        ++ "https://sh-dog-mags.big-data-biology.org/\n"
+        ++ "Data downloaded from the Urban Soil MAG Viewer:\n"
+        ++ "https://urban-soil-mags.big-data-biology.org/\n"
         ++ "\n"
         ++ "## Citation\n"
         ++ "\n"
-        ++ "Cusco, A., Duan, Y., Gil, F., Chklovski, A., Kruthi, N., Pan, S.,\n"
-        ++ "Forslund, S., Lau, S., Lober, U., Zhao, X.-M., and Coelho, L.P.\n"
-        ++ "\"Capturing global pet dog gut microbial diversity and hundreds of\n"
-        ++ "near-finished bacterial genomes by using long-read metagenomics in a\n"
-        ++ "Shanghai cohort\" (bioRxiv PREPRINT 2025)\n"
-        ++ "DOI: https://doi.org/10.1101/2025.09.17.676595\n"
+        ++ "Long-read metagenomic sequencing reveals novel lineages and "
+        ++ "functional diversity in urban soil microbiome Yiqian Duan, Anna Cuscó, "
+        ++ "Yaozhong Zhang, Juan S. Inda-Díaz, Chengkai Zhu, Alexandre Areias "
+        ++ "Castro, Xinrun Yang, Jiabao Yu, Gaofei Jiang, Xing-Ming Zhao, Luis "
+        ++ "Pedro Coelho\n"
+        ++ "bioRxiv 2026.03.20.713087; doi: https://doi.org/10.64898/2026.03.20.713087"
+
+
